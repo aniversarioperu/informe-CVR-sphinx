47786d9eb2305d125150d7fc1ce3ceb93d2cbd0d
aniversarioperu <yoni@aniversarioperu.me>
2015-09-30 10:57:44 +0300

    add context variable to conf

--- conf.py
+++ conf.py
@@ -109,7 +109,20 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'my_theme'
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    html_theme = 'my_theme'
+else:
+    html_theme = 'my_theme'
+    html_context = {
+        'css_files': [
+            '_static/my_style.css'
+        ],
+        'script_files': [
+            '_static/highlighter.js'
+        ],
+    }
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
